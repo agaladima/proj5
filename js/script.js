a843ba3fffd891ba75cdf2e633db8164fe6a1f6f
agaladima <arum.galadima@gmail.com>
2017-09-29 15:04:50 -0400

    getting there

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
-	$.ajax({
-	  url: 'https://randomuser.me/api/?results=12',
-	  dataType: 'json',
-	  success: function(data) {
-	    console.log(data);
-	  }
-	});
+	// $.ajax({
+	//   url: 'https://randomuser.me/api/?results=12',
+	//   dataType: 'json',
+	//   success: function(data) {
+	//     console.log(data);
+	//   }
+	// });
+
+	var url = 'https://randomuser.me/api/?results=12';
+	function displayEmployees(data) {
+		var employeeHTML = '<ul>';
+	    $.each(data.results, function(i, info) {
+	    	employeeHTML += '<div class="col">';
+	    	employeeHTML += '<img src="' + info.picture.large + '" class="image">';
+	    	employeeHTML += '<p class="name">' + info.name.first + info.name.last + '</p>';
+	    	employeeHTML += '<p class="email">' + info.email + '</p>';
+	    	employeeHTML += '<p class="city">' + info.location.city + '</p></div>';
+	    });
+	    employeeHTML += '</ul>'
+	    $('#employee-info').html(employeeHTML);
+	}
+	$.getJSON(url, displayEmployees);
 });
